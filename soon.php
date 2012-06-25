e2c446764dfb20b0c99b6ebe52d049f8d811acf5
elit3ge <dcbaldwin@gmail.com>
2012-06-25 16:20:39 +1000

    Fixed Header if Cat Empty

--- soon.php
+++ soon.php
@@ -4,8 +4,8 @@
 include 'config.php';
 
 // Feed URL
-    $feed = "http://".$ip."/api/".$api."/?cmd=future&sort=date&type=soon";
-    $feed2 = "http://".$ip."/api/".$api."/?cmd=future&sort=date&type=today";
+$feed = "http://".$ip."/api/".$api."/?cmd=future&sort=date&type=soon";
+$feed2 = "http://".$ip."/api/".$api."/?cmd=future&sort=date&type=today|missed";
     
 $sbJSON = json_decode(file_get_contents($feed));
 $sbJSON2 = json_decode(file_get_contents($feed2));
@@ -13,27 +13,51 @@ $sbJSON2 = json_decode(file_get_contents($feed2));
 // What are you!?
 echo "<h1>Airing Soon</h1>";
 
-// Run through each feed item
+// Remove Today header if no shows
+if ($sbJSON->{data}->{today} == "")
+{ }
+else
+{
 echo "<u>Today</u><br>";
+}
+
+// Run through each feed item
 foreach($sbJSON2->{data}->{today} as $show2) {
-    // Only grab shows of episode 1
 
-        // Reformat date
-        $newDate2 = date("l, j F Y", strtotime($show2->{airdate}));
+// Reformat date
+$newDate2 = date("l, j F Y", strtotime($show2->{airdate}));
 
-        // Show Details
-        echo "<a href='seasonlist.php?showid=".$show2->{tvdbid}."'>" . $show2->{show_name} . "</a>, S" . $show2->{season} . " E" . $show2->{episode} . " | " .$newDate2 . "<br />";
+// Show Details
+echo "<a href='seasonlist.php?showid=".$show2->{tvdbid}."'>" . $show2->{show_name} . "</a>, S" . $show2->{season} . " E" . $show2->{episode} . " | " .$newDate2 . "<br />";
 }
 
 echo "<br><u>Later</u><br>";
 foreach($sbJSON->{data}->{soon} as $show) {
-    // Only grab shows of episode 1
+// Only grab shows of episode 1
 
-        // Reformat date
-        $newDate = date("l, j F Y", strtotime($show->{airdate}));
+// Reformat date
+$newDate = date("l, j F Y", strtotime($show->{airdate}));
 
-        // Show Details
-        echo "<a href='seasonlist.php?showid=".$show->{tvdbid}."'>".$show->{show_name} . "</a>, S" . $show->{season} . " E" . $show->{episode} . " | " .$newDate . "<br />";
+// Show Details
+echo "<a href='seasonlist.php?showid=".$show->{tvdbid}."'>".$show->{show_name} . "</a>, S" . $show->{season} . " E" . $show->{episode} . " | " .$newDate . "<br />";
+}
+
+// Remove Today header if no shows
+if ($sbJSON->{data}->{missed} == "")
+{ }
+else
+{
+echo "<br><u>Missed</u><br>";
+}
+
+// Run through each feed item
+foreach($sbJSON2->{data}->{missed} as $show3) {
+
+// Reformat date
+$newDate3 = date("l, j F Y", strtotime($show3->{airdate}));
+
+// Show Details
+echo "<a href='seasonlist.php?showid=".$show2->{tvdbid}."'>" . $show3->{show_name} . "</a>, S" . $show3->{season} . " E" . $show3->{episode} . " | " .$newDate3 . "<br />";
 }
 
 ?>
