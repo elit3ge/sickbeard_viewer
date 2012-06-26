3f714afb90a19783c79449896d6486ed4faff46c
elit3ge <dcbaldwin@gmail.com>
2012-06-26 21:22:54 +1000

    Added CSS

--- footer.php
+++ footer.php
@@ -4,5 +4,6 @@ include 'version.php';
 echo "<br><br>";
 echo "Copyright (C) ".date(Y)." - ".$site_name." | <a href='http://github.com/elit3ge/sickbeard_viewer'>Project Page</a><br>";
 echo "Version: ".$version;
-
+// close css
+echo "</div>";
 ?>
--- header.php
+++ header.php
@@ -1,5 +1,9 @@
 <?php
 
+//start css
+echo "<link rel='stylesheet' type='text/css' href='style.css' />";
+echo "<div id='site_content'>";
+
 echo "<center>";
 echo "<h1>".$site_name."'s Collection</h1>";
 
@@ -13,5 +17,4 @@ else
 	echo "<a href='index.php'>Airing Soon</a> | <a href='shows.php'>Shows</a> | <a href='season.php'>New Seasons</a> | <a href='downloaded.php'>Recently Snatched</a>";
 }
 echo "</center>";
-
 ?>
