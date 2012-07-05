d6c4238ebb015decb319bb4f88582d621c96698d
elit3ge <dcbaldwin@gmail.com>
2012-07-05 19:01:48 +1000

    Changed header if

--- header.php
+++ header.php
@@ -4,17 +4,17 @@
 echo "<link rel='stylesheet' type='text/css' href='style.css' />";
 echo "<div id='site_content'>";
 
+//start center
 echo "<center>";
+// display title
 echo "<h1>".$site_name."'s Collection</h1>";
 
-// Check is sab is enabled
-if ($sab_enabled == "1")
-{
-	echo "<a href='index.php'>Airing Soon</a> | <a href='shows.php'>Shows</a> | <a href='season.php'>New Seasons</a> | <a href='downloaded.php'>Recently Snatched</a> | <a href='sabnzbd.php'>Download Queue</a>";
-}
-else
-{
-	echo "<a href='index.php'>Airing Soon</a> | <a href='shows.php'>Shows</a> | <a href='season.php'>New Seasons</a> | <a href='downloaded.php'>Recently Snatched</a>";
-}
-echo "</center>";
+echo "<a href='index.php'>Airing Soon</a> | <a href='shows.php'>Shows</a> | <a href='season.php'>New Seasons</a> | <a href='downloaded.php'>Recently Snatched</a>";
+
+if ($sab_enabled == "1") { echo " | <a href='sabnzbd.php'>Download Queue</a>"; } else {}
+if ($plex_enabled == "1") { echo " | <a href='plex-movies.php'>Plex</a>"; } else {}
+
+
+
+
 ?>
