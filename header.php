11d051c2378b4ee1cd6cc28870d0592acfc924b8
elit3ge <db@davids-air.home.gateway>
2014-04-09 20:27:13 +1000

    Toggle Sickbeard

--- header.php
+++ header.php
@@ -9,8 +9,7 @@ echo "<center>";
 // display title
 echo "<h1>".$site_name."'s Collection</h1>";
 
-echo "<a href='index.php'>Coming Soon</a> | <a href='shows.php'>Shows</a> | <a href='season.php'>New Seasons</a> | <a href='downloaded.php'>Recently Snatched</a>";
-
+if ($sickbeard_enabled == "1") { echo "<a href='index.php'>Airing Soon</a> | <a href='shows.php'>Shows</a> | <a href='season.php'>New Seasons</a> | <a href='downloaded.php'>Recently Snatched</a>"; } else {}
 if ($sab_enabled == "1") { echo " | <a href='sabnzbd.php'>Download Queue</a>"; } else {}
 if ($plex_enabled == "1") { echo " | <a href='plex-movies.php'>Plex</a>"; } else {}
 if ($headphones_enabled == "1") { echo " | <a href='headphones.php'>Headphones</a>"; } else {}
