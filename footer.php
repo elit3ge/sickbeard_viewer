b266a4062788e9a7e5282a9e804ab36607dfb8ce
elit3ge <db@davids-air.home.gateway>
2014-04-09 20:27:37 +1000

    Changed Project URL

--- footer.php
+++ footer.php
@@ -2,10 +2,11 @@
 
 include 'version.php';
 echo "<br><br>";
-echo "Copyright (C) ".date(Y)." - ".$site_name." | <a href='http://github.com/elit3ge/sickbeard_viewer'>Project Page</a><br>";
+echo "Copyright (C) ".date(Y)." - ".$site_name." | <a href='http://bit.ly/1jYt1q3'>Project Page</a><br>";
 echo "Version: ".$version;
 // close css
 echo "</div>";
 //close center
 echo "</center>";
+
 ?>
