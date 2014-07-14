bc74a02ad1db2730ff35ee0bb774bafc28f22dce
elit3ge <dcbaldwin@gmail.com>
2014-07-14 19:16:00 +1000

    Fixed Typo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $filename = 'config.php';
 if (file_exists($filename)) {
 
 include 'config.php';
-echo "<title>Coming Soon | ".$site_name."</title>";
+echo "<title>Airing Soon | ".$site_name."</title>";
 
 echo "<center>";
 	include 'header.php';
