d4473dc8d574a0ce6039e8507d42ac88794292ca
elit3ge <dcbaldwin@gmail.com>
2014-07-14 20:32:36 +1000

    Fixed an error if Sickbeard is disabled.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 include 'version.php';
 echo "<br><br>";
 echo "Copyright (C) ".date(Y)." - ".$site_name." | <a href='http://bit.ly/1jYt1q3'>Project Page</a><br>";
-echo "Version: ".$version;
+echo "<b>Version:</b> ".$version;
 // close css
 echo "</div>";
 //close center
--- index.php
+++ index.php
@@ -7,9 +7,10 @@ if (file_exists($filename)) {
 include 'config.php';
 echo "<title>Airing Soon | ".$site_name."</title>";
 
+
 echo "<center>";
 	include 'header.php';
-	include 'soon.php';
+	if ($sickbeard_enabled == "1") {include 'soon.php';} else {echo '<br>Sickbeard is Disabled in Config. Please Enable it to see the Airing Soon items.';}
 	include 'footer.php';
 echo "</center>";
 
