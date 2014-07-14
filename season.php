24fb9cfadb2c3f65d463b3aac2494aa50855233c
elit3ge <dcbaldwin@gmail.com>
2014-07-14 20:32:11 +1000

    Season List to a Table

--- season.php
+++ season.php
@@ -13,6 +13,7 @@ $sbJSON = json_decode(file_get_contents($feed));
 echo "<center>";
 include 'header.php';
 echo "<h1>New Season Start Dates</h1>";
+echo "<table style='width:auto'>";
 
 // Run through each feed item
 foreach($sbJSON->{data}->{later} as $show) {
@@ -23,9 +24,10 @@ foreach($sbJSON->{data}->{later} as $show) {
         $newDate = date("l, j F Y", strtotime($show->{airdate}));
 
         // Show Details
-        echo "<a href='episode.php?showid=".$show->{tvdbid}."&seasonid=".$show->{season}."'>".$show->{show_name} . ", Season " . $show->{season} . "</a> | " .$newDate . "<br />";
+        echo "<tr><td><a href='episode.php?showid=".$show->{tvdbid}."&seasonid=".$show->{season}."'>".$show->{show_name} . ", Season " . $show->{season} . "</a></td><td>" .$newDate . "</td></tr>";
     }
 }
+echo "</table>";
 include 'footer.php';
 echo "</center>";
 ?>
