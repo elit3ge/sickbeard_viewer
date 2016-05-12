019ac536c94caf3d28febf38e9925a2e92dc4f7e
elit3ge <dcbaldwin@gmail.com>
2016-05-12 18:56:52 +1000

    Added qualitybuttons

--- episode.php
+++ episode.php
@@ -57,7 +57,35 @@ foreach($sbJSON->{data} as $show) {
         echo "<button><a href='epdata.php?showid=".$showid."&seasonid=".$seasonid."&ep=".$counter."'><b>Episode:</b> " . $counter . "</a></button><br />";
         echo "<b>Name:</b> " . $show->{name} . "<br />";
         echo "<b>Aired:</b> " . $show->{airdate} . "<br />";
-        echo "<b>Quality:</b> " . $show->{quality} . " - Size: " . round($show->{file_size}/1024/1024,2) . " MB<br />";
+		echo "<b>Quality:</b> " . $show->{quality};
+		
+		if ($show->{quality} == "SD TV")
+		{
+			printf("<img src='images/SDTV.png'>");
+		}
+		elseif ($show->{quality} == "SD DVD")
+		{
+			printf("<img src='images/SDDVD.png'>");
+		}
+		elseif ($show->{quality} == "720p HDTV")
+		{
+			printf("<img src='images/720TV.png'>");
+		}
+		elseif ($show->{quality} == "1080p HDTV")
+		{
+			printf("<img src='images/1080TV.png'>");
+		}
+		elseif ($show->{quality} == "1080p WEB-DL")
+		{
+			printf("<img src='images/1080DL.png'>");
+		}
+		else
+		{
+			printf("<img src='images/unknown.png'>");
+		}
+		
+		echo " - Size: " . round($show->{file_size}/1024/1024,2) . " MB<br />";
+		
         if ($show->{status} == "Archived")
         {
         	echo "<b>Status:</b><font color='#41A317'> Archived </font><br /><br />";
@@ -68,7 +96,7 @@ foreach($sbJSON->{data} as $show) {
         }
         elseif ($show->{status} == "Downloaded")
         {
-        	echo "<b>Status:</b><font color='#92e49f'> Downloaded </font><br /><br />";
+			echo "<b>Status:</b><font color='#92e49f'> Downloaded </font><br /><br />";
         }
         elseif ($show->{status} == "Wanted")
         {
